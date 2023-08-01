8dcf7ebab5a7abc11a600cb160b608d8b571e51b
Reyner Fernaldi <reynerfernaldi@gmail.com>
2023-08-01 11:35:47 +0700

    Initial Commit

---
+++ Wani/WaniApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaniApp.swift
+//  Wani
+//
+//  Created by Reyner Fernaldi on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WaniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
