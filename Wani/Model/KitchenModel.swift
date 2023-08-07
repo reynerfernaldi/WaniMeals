c8b69153937ac70db16c2ad859c28ab8d421fa4d
Reyner Fernaldi <reynerfernaldi@gmail.com>
2023-08-07 11:11:41 +0700

    aiffah jelek

--- Wani/Model/KitchenModel.swift
+++ Wani/Model/KitchenModel.swift
@@ -13,27 +13,27 @@ struct MenuModel {
     
 }
 
-struct KitchenModel {
-    var customer: String
-    var Menu: [String]
-    var lengthInMinutes: Int
-    var theme: Theme
-}
-
-extension DailyScrum {
-    static let sampleData: [DailyScrum] =
-    [
-        DailyScrum(title: "Design",
-                   attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
-                   lengthInMinutes: 10,
-                   theme: .yellow),
-        DailyScrum(title: "App Dev",
-                   attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
-                   lengthInMinutes: 5,
-                   theme: .orange),
-        DailyScrum(title: "Web Dev",
-                   attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
-                   lengthInMinutes: 5,
-                   theme: .poppy)
-    ]
-}
+//struct KitchenModel {
+//    var customer: String
+//    var Menu: [String]
+//    var lengthInMinutes: Int
+//    var theme: Theme
+//}
+//
+//extension DailyScrum {
+//    static let sampleData: [DailyScrum] =
+//    [
+//        DailyScrum(title: "Design",
+//                   attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+//                   lengthInMinutes: 10,
+//                   theme: .yellow),
+//        DailyScrum(title: "App Dev",
+//                   attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+//                   lengthInMinutes: 5,
+//                   theme: .orange),
+//        DailyScrum(title: "Web Dev",
+//                   attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
+//                   lengthInMinutes: 5,
+//                   theme: .poppy)
+//    ]
+//}
