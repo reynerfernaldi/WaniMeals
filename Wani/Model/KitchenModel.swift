6ee0da315bcd1b38ed737822508e714cce27b172
Reyner Fernaldi <reynerfernaldi@gmail.com>
2023-08-07 11:07:57 +0700

    init

---
+++ Wani/Model/KitchenModel.swift
@@ -0,0 +1,39 @@
+//
+//  KitchenModel.swift
+//  Wani
+//
+//  Created by Reyner Fernaldi on 07/08/23.
+//
+
+import Foundation
+
+struct MenuModel {
+    var name : String
+    var quantity : Int
+    
+}
+
+struct KitchenModel {
+    var customer: String
+    var Menu: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design",
+                   attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+                   lengthInMinutes: 10,
+                   theme: .yellow),
+        DailyScrum(title: "App Dev",
+                   attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+                   lengthInMinutes: 5,
+                   theme: .orange),
+        DailyScrum(title: "Web Dev",
+                   attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
+                   lengthInMinutes: 5,
+                   theme: .poppy)
+    ]
+}
