c893afeadc5d27139519cb5df1bc85d14429d07d
Aiffah Kiysa Waafi <aiffahwaafi6@gmail.com>
2023-08-24 11:56:04 +0700

    fix app clip

--- WaniAppClip/ContentView.swift
+++ WaniAppClip/ContentView.swift
@@ -2,16 +2,34 @@
 //  ContentView.swift
 //  WaniAppClip
 //
-//  Created by Aiffah Kiysa Waafi on 21/08/23.
+//  Created by Aiffah Kiysa Waafi on 24/08/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var foodList: [Food] = [
+        Food(name: "Bakso", price: 10000, picture: "x.png"),
+        Food(name: "Nasi Ayam Geprek", price: 12000, picture: "x.png"),
+        Food(name: "Nasi Udang Geprek", price: 15000, picture: "x.png"),
+        Food(name: "Nasi Ayam Kremes", price: 15000, picture: "x.png"),
+        Food(name: "Indomie", price: 6000, picture: "x.png")
+    ]
+    
     var body: some View {
-//        CommonView()
-        VStack{
-            Text("ini appclip")
+        NavigationStack{
+            VStack(alignment: .leading) {
+                VStack{
+                    ForEach($foodList, id: \.id) { x in
+                        Card(item: x)
+                    }
+                }
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            .background(Color("ColorBackground"))
+            .navigationTitle("Menu")
+            .navigationBarTitleDisplayMode(.large)
         }
     }
 }
